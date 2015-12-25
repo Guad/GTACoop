9962271f911ed8192c59b216e82f6dbcc338b986
butich <butichex@gmail.com>
2015-12-25 17:47:22 +0300

    Added CreateClientMenu() to create menu using NativeUI on client by server function.

---
+++ Client/MenuData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf;
+
+namespace GTACoOp
+    {
+        [ProtoContract]
+        public class MenuOpen
+        {
+        [ProtoMember(1)]
+        public bool Open;
+        }
+    }
---
+++ Server/RemoteMenuData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+using System.Runtime.CompilerServices;
+
+namespace GTAServer
+{
+    [ProtoContract]
+    public class RemoteMenuData
+    {
+       [ProtoMember(1)]
+       public  _UIMenu _uiMenu;
+       [ProtoMember(2)]
+       public  List<_UIMenuItem> _uiMenuItem;
+        [ProtoMember(3)]
+        public List<_UIMenuCheckboxItem> _uiMenuCheckboxItem;
+        [ProtoMember(4)]
+        public List<_UIMenuListItem> _uiMenuListItem;
+
+    }
+    [ProtoContract]
+    public class _UIMenu
+    {
+        [ProtoMember(1)]
+        public string title;
+        [ProtoMember(2)]
+        public string subtitle;
+    }
+    [ProtoContract]
+    public class _UIMenuItem
+    {
+        [ProtoMember(1)]
+        public string name;
+        [ProtoMember(2)]
+        public string description;
+    }
+    [ProtoContract]
+    public class _UIMenuCheckboxItem
+    {
+        [ProtoMember(1)]
+        public string name;
+        [ProtoMember(2)]
+        public string description;
+        [ProtoMember(3)]
+        public bool check;
+    }
+    [ProtoContract]
+    public class _UIMenuListItem
+    {
+        [ProtoMember(1)]
+        public string name;
+        [ProtoMember(2)]
+        public string description;
+        [ProtoMember(3)]
+        public List<string> list;
+        [ProtoMember(4)]
+        public int index;
+    }
+}
