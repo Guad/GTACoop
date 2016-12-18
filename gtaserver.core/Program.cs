673a0c26e09e283a6dbdfb095ea5efdcaa8ea116
Mitchell Monahan <soccermitchy@gmail.com>
2016-12-17 22:57:51 -0500

    Server now has a config file.. and the file is in .gitignore

--- gtaserver.core/Program.cs
+++ gtaserver.core/Program.cs
@@ -1,34 +1,53 @@
 ﻿using System;
 using System.Threading;
+using System.IO;
+using System.Xml.Serialization;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace GTAServer
 {
     public class Program
     {
+        public static ServerConfiguration GameServerConfiguration;
+        private static ILogger _logger;
+
         public static void Main(string[] args)
         {
             Util.LoggerFactory = new LoggerFactory()
 #if DEBUG
                 .AddConsole(LogLevel.Trace)
+                .AddDebug(); // this adds stuff to VS debug console
 #else
-                .AddConsole()
+                .AddConsole();
 #endif
-                .AddDebug();
-            var logger = Util.LoggerFactory.CreateLogger<Program>();
+
+            _logger = Util.LoggerFactory.CreateLogger<Program>();
 #if DEBUG
-            logger.LogWarning("Note - This build is a debug build. Please do not share this build and report any issues to Mitchell Monahan (@wolfmitchell)");
-            logger.LogWarning("Furthermore, debug builds will not announce themselves to the master server, regardless of the AnnounceSelf config option.");
-            logger.LogWarning("To help bring crashes to the attention of the server owner and make sure they are reported to me, error catching has been disabled in this build.");
+            _logger.LogWarning("Note - This build is a debug build. Please do not share this build and report any issues to Mitchell Monahan (@wolfmitchell)");
+            _logger.LogWarning("Furthermore, debug builds will not announce themselves to the master server, regardless of the AnnounceSelf config option.");
+            _logger.LogWarning("To help bring crashes to the attention of the server owner and make sure they are reported to me, error catching has been disabled in this build.");
 #endif
-            logger.LogInformation("Server preparing to start...");
+            _logger.LogInformation("Reading server configuration...");
+            GameServerConfiguration = LoadServerConfiguration("serverSettings.xml");
+            _logger.LogInformation("Configuration loaded...");
 
-            var gameServer = new GameServer(4699, "GTAServer .NET Core Test", "freeroam");
+            _logger.LogInformation("Server preparing to start...");
 
-            logger.LogInformation("Server starting...");
+            var gameServer = new GameServer(GameServerConfiguration.Port, GameServerConfiguration.ServerName,
+                GameServerConfiguration.GamemodeName)
+            {
+                Password = GameServerConfiguration.Password,
+                MasterServer = GameServerConfiguration.PrimaryMasterServer,
+                AnnounceSelf = GameServerConfiguration.AnnounceSelf,
+                AllowNicknames = GameServerConfiguration.AllowNicknames,
+                AllowOutdatedClients = GameServerConfiguration.AllowOutdatedClients,
+            };
+
+            _logger.LogInformation("Server starting...");
             gameServer.Start();
 
-            logger.LogInformation("Starting server main loop, ready to accept connections.");
+            _logger.LogInformation("Starting server main loop, ready to accept connections.");
             while (true)
             {
 #if DEBUG
@@ -45,6 +64,26 @@ namespace GTAServer
 #endif
                 Thread.Sleep(1);
             }
+        }
+
+        public static ServerConfiguration LoadServerConfiguration(string path)
+        {
+            var ser = new XmlSerializer(typeof(ServerConfiguration));
+
+            ServerConfiguration cfg = null;
+            if (File.Exists(path))
+            {
+                using (var stream = File.OpenRead(path)) cfg = (ServerConfiguration)ser.Deserialize(stream);
+                using (
+                    var stream = new FileStream(path, File.Exists(path) ? FileMode.Truncate : FileMode.Create,
+                        FileAccess.ReadWrite)) ser.Serialize(stream, cfg);
+            }
+            else
+            {
+                _logger.LogInformation("No config found, creating a new one");
+                using (var stream = File.OpenWrite(path)) ser.Serialize(stream, cfg = new ServerConfiguration());
+            }
+            return cfg;
         }
     }
 }
