8650338b38eaa8b0016ab2f4475f7d7a801de83a
Mitchell Monahan <soccermitchy@gmail.com>
2016-12-18 14:45:43 -0500

    Add test plugin

---
+++ gtaserver.core-plugins/TestPlugin/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GTAServer;
+using GTAServer.PluginAPI;
+
+namespace TestPlugin
+{
+    public class TestPlugin : IPlugin
+    {
+        public string Name => "Test Plugin";
+
+        public string Description
+            => "This plugin is used to test the plugin-loading capabilities of the new server. Does nothing.";
+
+        public string Author => "Mitchell Monahan (wolfmitchell)";
+
+        public bool OnEnable(GameServer gameServer, bool isAfterServerLoad)
+        {
+            return true; // successful load
+        }
+    }
+}
