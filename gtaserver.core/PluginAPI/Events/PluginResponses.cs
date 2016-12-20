e60e908250e58abba5d449640ae2fac8a332447d
Mitchell Monahan <soccermitchy@gmail.com>
2016-12-20 14:33:14 -0500

    Add second data field class, fix typo

--- gtaserver.core/PluginAPI/Events/PluginResponses.cs
+++ gtaserver.core/PluginAPI/Events/PluginResponses.cs
@@ -5,21 +5,6 @@ using System.Threading.Tasks;
 
 namespace GTAServer.PluginAPI.Events
 {
-    public class PluginResponse<T>
-    {
-        /// <summary>
-        /// If the event should continue being processed by other plugins
-        /// </summary>
-        public bool ContinuePluginProc;
-        /// <summary>
-        /// If the event should continue being processed by the server
-        /// </summary>
-        public bool ContinueServerProc;
-        /// <summary>
-        /// Data specific to the event time, usually specified in the event.
-        /// </summary>
-        public T Data;
-    }
 
     public class PluginResponse
     {
@@ -31,5 +16,21 @@ namespace GTAServer.PluginAPI.Events
         /// If the event should continue being processed by the server
         /// </summary>
         public bool ContinueServerProc;
+    }
+
+    public class PluginResponse<T> : PluginResponse
+    {
+        /// <summary>
+        /// Data specific to the event type, usually specified in the event.
+        /// </summary>
+        public T Data;
+    }
+
+    public class PluginResponse<T1, T2> : PluginResponse<T1>
+    {
+        /// <summary>
+        /// Second data field specific to event
+        /// </summary>
+        public T2 Data2;
     }
 }
